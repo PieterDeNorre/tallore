1969c92d67ca1366e9647a26750e7fb7294eca99
Pieter De Norre <pieter.de.norre@telenet.be>
2015-03-09 11:41:07 +0100

    v1.1

--- index.php
+++ index.php
@@ -1,41 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-<head>
-	<meta http-equiv='content-type' content='text/html; charset=utf-8' />
-	<meta name='description' content='' />
-	<meta name='keywords' content='' />
-	<meta name='author' content='' />
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Tallore</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" href="css/reset.css" />
-	<link rel="stylesheet" href="css/index.css" />
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
-	<script type="text/javascript" src="js/live.js"></script>
-	
-
-<body>
-	<div id="container" class="header"><?php include 'includes/header.php' ?></div>
-	<div id="container" class="map"><?php include 'includes/map.php' ?></div>
-
-
-
-
-
-	<div id="container" class="menu">
-		<?php include 'includes/contentmenu.php' ?>
-	</div>
-	<div id="container" class="about">
-		<?php include 'includes/aboutus.php' ?>
-	</div>
-	<div id="container" class="contact">
-		<?php include 'includes/contact.php' ?>
-	</div>
-</body>
-</html>
+<?php
+	header("Location: https://www.facebook.com/TalloreSoups");
+	exit;
+?>
